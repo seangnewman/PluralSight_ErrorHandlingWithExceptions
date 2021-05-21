2345a4a58ebe1891bb1e97612b863c3504b7fb2f
seangnewman <seangnewman@msn.com>
2021-05-21 08:57:24 -0700

    Add project files.

---
+++ ConsoleCalculator/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleCalculator
+{
+    public class Calculator
+    {
+        public int Calculate(int number1, int number2, string operation)
+        {
+            if (operation == "/")
+            {
+                return Divide(number1, number2);
+            }
+            else
+            {            
+                Console.WriteLine("Unknown operation.");
+                return 0;
+            }
+        }
+        
+        private int Divide(int number, int divisor)
+        {
+            return number / divisor;
+        }
+    }
+}
+
---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter first number");
+            int number1 = int.Parse(ReadLine());
+
+            WriteLine("Enter second number");
+            int number2 = int.Parse(ReadLine());
+
+            WriteLine("Enter operation");
+            string operation = ReadLine().ToUpperInvariant();
+
+
+            var calculator = new Calculator();
+            int result = calculator.Calculate(number1, number2, operation);
+            DisplayResult(result);
+
+
+            
+            WriteLine("\nPress enter to exit.");
+            ReadLine();
+        }
+
+        private static void DisplayResult(int result)
+        {
+            WriteLine($"Result is: {result}");
+        }
+    }
+}
